9077df7efba48561b3c79126cff2bf7cd42e7287
kirbdee <kirby.domingo@gmail.com>
2015-07-31 00:03:35 -0700

    Added initial files and functionality for cheepy extension

---
+++ background.js
@@ -0,0 +1,34 @@
+/**
+ * Created by kirbdee on 7/30/15.
+ */
+chrome.webRequest.onBeforeRequest.addListener(
+  function(req) {
+    //Catch URL and attach code
+    var url = document.createElement('a');
+    url.href = req.url;
+
+    switch(url.hostname){
+      case 'www.amazon.com':
+        if( url.search.length > 0 && url.search.indexOf(affCodes.amazon) == -1){
+          url.search+='&'+affCodes.amazon;
+        }
+        break;
+      case 'www.bestbuy.com':
+        if( url.search.length > 0 && url.search.indexOf(affCodes.bestbuy) == -1){
+          url.search+='&'+affCodes.bestbuy;
+        }
+        break;
+      case 'www.walmart.com':
+        if( url.search.length > 0 && url.search.indexOf(affCodes.walmart) == -1){
+          url.search+='&'+affCodes.walmart;
+        }
+        break;
+    }
+
+    return {redirectUrl: url.href}
+  },{
+    urls: domains,
+    types: ["main_frame"]
+  },
+  ["blocking"]
+);
---
+++ domains.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kirbdee on 7/30/15.
+ */
+var domains = [
+  "*://*.amazon.com/*",
+  "*://*.bestbuy.com/*",
+  "*://*.walmart.com/*"
+];
+
+var affCodes = {
+  "amazon" : "tag=123QWE",
+  "bestbuy" : "LID=123QWE",
+  "walmart" : "WMC=123QWE"
+}
